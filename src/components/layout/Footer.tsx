65d51b44a8ca8e72843ac6d4b78af5836bef1935
kminhoo <whdgus4158@gmail.com>
2024-12-17 18:56:51 +0900

    feat: Footer 컴포넌트 반응형 및 기능

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white dark:bg-slate-800">
+      <div className="max-w-7xl m-auto p-3">
+        <p className="text-slate-900 dark:text-white text-center text-sm md:text-base">
+          @ Copyright 2024 <span className="text-blue-300 dark:text-yellow-300">Minus.</span> All rights reserved
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
