4d392b7b25282e520c0e3c5fe465156dab86e7d4
kminhoo <whdgus4158@gmail.com>
2024-12-18 20:25:03 +0900

    test: 2초 지연 테스트 추가

--- src/utils/riotApi.ts
+++ src/utils/riotApi.ts
@@ -1,13 +1,16 @@
 import { Champion } from '@tsc/champions'
 
 export const getChampionRotation = async (): Promise<Champion[]> => {
+  // 2초 지연 추가
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
   const response = await fetch('/api/rotation')
 
   if (!response.ok) {
     throw new Error('로테이션 챔피언 데이터를 가져오지 못했습니다.')
   }
 
-  const data = await response.json()
+  const data: Champion[] = await response.json()
 
   return data
 }
